3d7da2914ee7888782b6c52ad026c6ad7bf00dd3
Nazira <n.umucyo@alustudent.com>
2025-02-21 05:45:37 -0800

    concat

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import sys
-str1 = sys.argv[1]  # First part from the command line argument
-str2 = sys.argv[2]  # Second part from the command line argument
-print(f"{str1} {str2}")
+str1 = 'object-oriented'
+str2 = 'programming with Python'
+print(f'{str1} {str2}')
