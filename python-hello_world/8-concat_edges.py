3e40297fe4979d25b629db0933e9f1b27fa4f1e9
Nazira <n.umucyo@alustudent.com>
2025-02-21 05:18:51 -0800

    edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = input()  # Dynamically input the value
-str2 = input()  # Dynamically input the value
-print(f"{str1} {str2}")
+str1 = "object-oriented"
+str2 = "programming with Python"
+print(f"{str1} {str2}")  # Concatenating the variables
