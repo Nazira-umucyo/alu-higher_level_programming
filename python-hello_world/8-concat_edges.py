28cf4e57c082941daf7f789a02b6cf1c248d4bd4
Nazira <n.umucyo@alustudent.com>
2025-02-21 05:32:55 -0800

    edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = input()  # Dynamically input the first part of the string
-str2 = input()  # Dynamically input the second part of the string
+str1 = input()  # Expect input for first part
+str2 = input()  # Expect input for second part
 print(f"{str1} {str2}")
