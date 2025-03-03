765b9b31516ceccb748f5a5a08b232ab2ef4b8d0
Nazira <n.umucyo@alustudent.com>
2025-03-03 04:55:46 -0800

    noted

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = 98  # The integer value
-print(f"{number} Battery street")  # Use f-string to print the integer and string
+number = 98
+print(f"{number:d} Battery street")
