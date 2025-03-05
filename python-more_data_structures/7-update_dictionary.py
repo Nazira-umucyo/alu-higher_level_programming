e1c1554c1658795f35855d7e49ee0e121b9935a2
Nazira <n.umucyo@alustudent.com>
2025-03-05 12:47:39 -0800

    score

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    """Print a dictionary by ordered keys."""
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
-    a_dictionary[key] = value
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
     return (a_dictionary)
