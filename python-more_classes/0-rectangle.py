facc2b0761963da9fd71048e509bec9c9a727708
Nazira <n.umucyo@alustudent.com>
2025-03-07 04:49:27 -0800

    rec

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a class Rectangle.
+The class is empty and serves as a starting point for future extensions
+where properties and methods for the rectangle can be added.
+"""
 
 class Rectangle:
     pass
