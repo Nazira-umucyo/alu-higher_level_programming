cd928d218422b96830efb8c2b71b5d131b414a01
Nazira <n.umucyo@alustudent.com>
2025-03-07 05:11:25 -0800

    tot

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -4,8 +4,6 @@ This module defines a class Rectangle that has private attributes width and heig
 It also provides getter and setter methods for both attributes, validation, 
 and methods to calculate the area and perimeter of the rectangle.
 """
-
-
 class Rectangle:
     """
     A class to represent a rectangle.
