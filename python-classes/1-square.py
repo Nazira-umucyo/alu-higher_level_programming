7cb24f79cf8c89b9f2db1f3204fb840f32837ef0
Nazira <n.umucyo@alustudent.com>
2025-03-07 01:57:30 -0800

    delete

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-
 class Square:
     """Defines a square with a private size attribute."""
-
+    
     def __init__(self, size):
         """Initializes a new square with a given size."""
         self.__size = size
-
