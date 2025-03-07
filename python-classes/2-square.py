bc714af918d30c57642123ef1adcea25024db9a0
Nazira <n.umucyo@alustudent.com>
2025-03-07 02:20:34 -0800

    root

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+"""This module defines a class Square with a private size attribute.
+It includes validation to ensure that size is an integer and greater than or equal to 0.
+"""
+
 class Square:
     """Represents a square with a private size attribute."""
-    
+
     def __init__(self, size=0):
         """Initializes the square with a given size, which defaults to 0."""
         if not isinstance(size, int):
