786ece6716a880bd7cc9e75576d0428c76293a1b
Nazira <n.umucyo@alustudent.com>
2025-03-07 02:00:39 -0800

    again

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,7 +2,8 @@
 
 class Square:
     """Defines a square with a private size attribute."""
-    
+
     def __init__(self, size):
         """Initializes a new square with a given size."""
         self.__size = size
+
