60362e04b59a227c627113d55259b6b1654960ce
Nazira-umucyogit config --global user.email n.umucyo@alustudent.com <n.umucyo@alustudent.com>
2025-06-10 05:11:52 -0700

    md

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (typeof w !== 'number' || w <= 0 || typeof h !== 'number' || h <= 0) {
+      // Do not set any properties, leaving the object empty
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
