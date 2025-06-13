6f1862dcb5336f216d7fb5f6372f892753a8b3f2
Nazira-umucyo <n.umucyo@alustudent.com>
2025-06-13 19:20:24 +0000

    circle

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,15 @@
-// File: 0-readme.js
+#!/usr/bin/node
+
 const fs = require('fs');
 
+// Get file path from command line argument
 const filePath = process.argv[2];
 
+// Read the file asynchronously in utf-8 encoding
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
-    return;
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
