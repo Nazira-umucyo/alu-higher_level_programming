43a704a8eda8b4001ed15e1cc7a626938ef3159f
Nazira-umucyo <n.umucyo@alustudent.com>
2025-06-13 19:26:00 +0000

    circle

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,12 @@
-// File: 1-writeme.js
+#!/usr/bin/node
+
 const fs = require('fs');
 
+// Get the file path and content from command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
+// Write the content to the file using utf-8 encoding
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.log(err);
